48b10b33a5d7b705fe8691cc29863d6964297ba6
dumpling <dumpling@abc.com>
2020-09-15 19:33:11 +0800

    Update LoginController

--- src/chatapp/controllers/LoginController.java
+++ src/chatapp/controllers/LoginController.java
@@ -1,34 +1,69 @@
 package chatapp.controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 import chatapp.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.util.regex.Pattern;
 
 public class LoginController {
 
     @FXML
-    TextField fIPAddress, fPort;
+    TextField fIPAddress, fPort, fUsername;
 
     @FXML
-    private void onLoginClick() throws Exception {
+    Button btnLogin;
+
+    @FXML
+    HBox hbUsername, hbIP, hbPort;
+
+    @FXML
+    public void login() throws Exception {
+        String username = fUsername.getText();
+        String ip = fIPAddress.getText();
+        String port = fPort.getText();
+
         if (isValidLogin()) {
-            Main.changeScene("views/matching.fxml");
-//            TODO: View matching screen
-//            TODO: Once client 2 has connected, change scene to chat
+            Main.changeScene("views/chat.fxml");
         } else {
-//            TODO: Add error messages for invalid field/s
+            if (!isValidUsername(username)) {
+                createErrorMessage(hbUsername, "  Please enter a username.");
+            }
+
+            if (!isValidIP(ip)) {
+                createErrorMessage(hbIP, "  Please enter a valid IP Address.");
+            }
+
+            if (!isValidPort(port)) {
+                createErrorMessage(hbPort, "  Please enter a valid port number.");
+            }
         }
+    }
+
+    private void createErrorMessage(HBox parent, String message) {
+        Label err = new Label(message);
+        err.getStyleClass().addAll("error", "label", "italic");
+        parent.getChildren().add(err);
+    }
+
+    private boolean isValidUsername(String name) {
+        return name.length() > 0;
     }
 
     private boolean isValidIP(String ipString) {
         String regex = "(([0-1]?[0-9]{1,2}\\.)|(2[0-4][0-9]\\.)|(25[0-5]\\.)){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))";
         Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(ipString).matches();
+        return pattern.matcher(ipString).matches() || ipString.equalsIgnoreCase("localhost");
     }
 
-    private boolean isValidPort(int port) {
+    private boolean isValidPort(String portString) {
+        if (portString.length() == 0)
+            return false;
+
+        int port = Integer.parseInt(portString);
         return port >= 0 && port <= 65535;
     }
 
@@ -37,9 +72,10 @@ public class LoginController {
             return false;
         }
 
+        String username = fUsername.getText();
         String ip = fIPAddress.getText();
-        int port = Integer.parseInt(fPort.getText());
+        String port = fPort.getText();
 
-        return isValidIP(ip) && isValidPort(port);
+        return isValidUsername(username) && isValidIP(ip) && isValidPort(port);
     }
 }
